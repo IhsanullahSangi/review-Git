d0eb76138580ab0d66b468b1a20cfe8d8125d215
IhsanullahSangi <ihsanullah_stud@yahoo.com>
2025-02-06 19:36:41 +0500

    This is first version

---
+++ MarksCal.js
@@ -0,0 +1,11 @@
+let score =70;
+
+if (score >=80){
+    console.log("A Grade")
+}else if(score >=60){
+    console.log ("B Grade")
+}else if(score >=50){
+    console.log("C Grade")
+}else {
+    console.log("Fail")
+}
---
+++ bootCamp.js
@@ -0,0 +1,9 @@
+let numberOfGuest = 5
+
+if (numberOfGuest <= 2){
+    console.log("Small")
+}else if(numberOfGuest <=5 ){
+    console.log("Medium")
+}else{
+    console.log ("Large")
+}
