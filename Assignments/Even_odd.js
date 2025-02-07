b6eb1d53fbac344649c63635ea9293fe90708a19
IhsanullahSangi <ihsanullah_stud@yahoo.com>
2025-02-07 07:51:31 +0500

    New

---
+++ Assignments/Even_odd.js
@@ -0,0 +1,11 @@
+function checkEvenOdd(num) {
+    // Return "Even" if num is even, otherwise return "Odd"
+  
+    if (num % 2 ==  0){
+      console.log(`Number ${num} is even`)
+    }else{
+      console.log(`Number ${num} is odd`)
+    }
+}
+
+checkEvenOdd(6);
